d2e97c7f66ec81cf483dd557caa9952a1c26a87c
Cody Joseph Krainock <codykrainock@gmail.com>
2021-08-08 19:34:16 -0700

    Simply ascii board code

--- src/board/ui.rs
+++ src/board/ui.rs
@@ -3,23 +3,41 @@ use super::Board;
 
 impl Board {
     pub fn to_ascii(&self) -> String {
-        let divider = "+---+---+---+---+---+---+---+---+\n";
-        let rows: Vec<String> = (0..=7)
-            .rev()
-            .map(|row| {
-                let cells: Vec<String> = (0..=7)
-                    .map(|col| {
-                        let square = Square::from_row_col(row, col);
-                        match self.get(square) {
-                            Some((piece, color)) => piece.to_fen(color),
-                            None => ' ',
-                        }
-                    })
-                    .map(|ch| ch.to_string())
-                    .collect();
-                format!("| {} |\n", cells.join(" | "))
-            })
-            .collect();
-        format!("{}{}{}", divider, rows.join(divider), divider)
+        let divider = "+---+---+---+---+---+---+---+---+";
+        let files: [char; 8] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+        let ranks: [char; 8] = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+        let mut rows: Vec<String> = vec![];
+
+        let row_iter = (0..8).rev();
+        let col_iter = 0..8;
+
+        for row in row_iter {
+            let mut cells: Vec<String> = vec![];
+            for col in col_iter.clone() {
+                let sq = Square::from_row_col(row, col);
+                let cell = match self.get(sq) {
+                    Some((piece, color)) => piece.to_fen(color),
+                    None => ' ',
+                };
+                cells.push(cell.to_string());
+            }
+            let formatted_cells = format!("| {} |", cells.join(" | "));
+
+            rows.push(format!("{} {}", ' ', divider));
+            rows.push(format!("{} {}", ranks[row], formatted_cells));
+        }
+        rows.push(format!("{} {}", ' ', divider));
+        let formatted_ranks_footer = format!(
+            "  {}  ",
+            files
+                .iter()
+                .map(|c| c.to_string())
+                .collect::<Vec<String>>()
+                .join("   ")
+        );
+        rows.push(format!("{} {}", ' ', formatted_ranks_footer));
+
+        rows.join("\n")
     }
 }
