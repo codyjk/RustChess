558287b147a40e368cdd3feb2193483ce79cc286
Cody Joseph Krainock <codykrainock@gmail.com>
2021-08-29 22:03:36 -0700

    Better input handling error type

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -3,18 +3,27 @@ use std::io;
 use crate::board::square;
 use crate::game::command::{Command, MakeMove};
 use regex::Regex;
+use thiserror::Error;
 
-pub fn parse_command() -> Result<Box<dyn Command>, String> {
+#[derive(Error, Debug)]
+pub enum InputError {
+    #[error("io error: {error:?}")]
+    IOError { error: String },
+    #[error("invalid input: {input:?}")]
+    InvalidInput { input: String },
+}
+
+pub fn parse_command() -> Result<Box<dyn Command>, InputError> {
     let mut input = String::new();
     let raw = match io::stdin().read_line(&mut input) {
         Ok(_n) => input.trim_start().trim_end(),
-        Err(error) => return Err(format!("io error: {}", error)),
+        Err(error) => return Err(InputError::IOError { error: error.to_string() }),
     };
 
     let re = Regex::new("^([a-h][1-8])([a-h][1-8])$").unwrap();
     let caps = match re.captures(&raw) {
         Some(captures) => captures,
-        None => return Err(format!("invalid input: {}", raw)),
+        None => return Err(InputError::InvalidInput { input: raw.to_string() }),
     };
 
     let command = MakeMove::new(
