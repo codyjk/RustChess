6531058753f1fb6d7a457bc9b9fcf5c03c507ca4
Cody Joseph Krainock <me@codyjoseph.com>
2022-03-11 18:02:44 -0800

    Add move generation benchmark

---
+++ benches/movegen_benchmark.rs
@@ -0,0 +1,27 @@
+use chess::board::color::Color;
+use chess::board::piece::Piece;
+use chess::board::{square, Board};
+use chess::moves;
+use chess::moves::targets::Targets;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let targets = &Targets::new();
+    c.bench_function("generate rook moves", |b| {
+        b.iter(|| test_generate_rook_moves(targets))
+    });
+}
+
+fn test_generate_rook_moves(targets: &Targets) {
+    let mut board = Board::new();
+    board.put(square::A3, Piece::Pawn, Color::White).unwrap();
+    board.put(square::H3, Piece::Pawn, Color::Black).unwrap();
+    board.put(square::C3, Piece::Rook, Color::White).unwrap();
+    board.put(square::C1, Piece::King, Color::White).unwrap();
+    board.put(square::C7, Piece::Pawn, Color::White).unwrap();
+    moves::generate_rook_moves(&board, Color::White, targets);
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
