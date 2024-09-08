d9a841a8f90eb0285e6b1df067afa26e911d349e
Cody Joseph Krainock <me@codyjoseph.com>
2024-09-08 10:40:03 -0700

    More descriptive CLI

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,40 @@ use chess::game::modes::{
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "chess", about = "chess engine cli")]
+#[structopt(
+    name = "chess",
+    about = "A classical chess engine implemented in Rust ♛"
+)]
 enum Chess {
+    #[structopt(
+        name = "count-positions",
+        about = "Count the number of possible positions for a given `--depth` (default: 4), and reports the time it took to do so. By default, this searches all possible positions. The routine can be run with alpha-beta pruning by selecting `--strategy alpha-beta`."
+    )]
     CountPositions {
         #[structopt(short, long, default_value = "4")]
         depth: u8,
         #[structopt(short, long, default_value = "all")]
         strategy: CountPositionsStrategy,
     },
+    #[structopt(
+        name = "play",
+        about = "Play a game against the computer, which will search for the best move using alpha-beta pruning at the given `--depth` (default: 4). Your starting color will be chosen at random unless you specify with `--color`."
+    )]
     Play {
         #[structopt(short, long, default_value = "4")]
         depth: u8,
         #[structopt(short = "c", long = "color", default_value = "random")]
         color: Color,
     },
+    #[structopt(
+        name = "pvp",
+        about = "Play a game against another human on this local machine."
+    )]
     Pvp,
+    #[structopt(
+        name = "watch",
+        about = "Watch the computer play against itself at the given `--depth` (default: 4)."
+    )]
     Watch {
         #[structopt(short, long, default_value = "4")]
         depth: u8,
